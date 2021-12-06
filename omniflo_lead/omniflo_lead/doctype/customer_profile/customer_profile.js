8370f31b7bb0d78b7cc16263f5dedff5de5a629b
vivek <techomniflo>
2021-12-06 19:40:49 +0530

    Customer Profile doctype created

---
+++ omniflo_lead/omniflo_lead/doctype/customer_profile/customer_profile.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Omniflo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Customer Profile', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ omniflo_lead/omniflo_lead/doctype/customer_profile/test_customer_profile.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, Omniflo and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestCustomerProfile(unittest.TestCase):
+	pass
