c52afe10c734229aea8e5ceede9bebf47bf73030
vivek <techomniflo>
2022-03-26 00:06:13 +0530

    audit log items added

---
+++ omniflo_lead/omniflo_lead/doctype/audit_log/audit_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Omniflo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Audit Log', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ omniflo_lead/omniflo_lead/doctype/audit_log/test_audit_log.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Omniflo and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestAuditLog(unittest.TestCase):
+	pass
