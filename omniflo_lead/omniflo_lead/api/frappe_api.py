d98e0732b8b5f9bd08a9a1bf49d75e0dd6dce77b
gourav-saini-omniflo <gourav.saini@omniflo.in>
2022-06-27 01:12:46 +0530

    image approve has been made and  time series gmv api has been made

--- omniflo_lead/omniflo_lead/api/frappe_api.py
+++ omniflo_lead/omniflo_lead/api/frappe_api.py
@@ -181,3 +181,72 @@ def time_series_gmv():
 	for i,date in enumerate(list_of_date):
 		di[date]=crate_dict_for_audit(date,audit_data)
 	return di
+@frappe.whitelist()
+def time_series_gmv():
+	values={"brand":frappe.request.args["brand"]}
+	audit_data=frappe.db.sql("""select (DATE_FORMAT(al.posting_date,'%%d-%%m-%%y')) as date,al.customer,ali.current_available_qty as qty,ali.item_name,i.mrp from `tabAudit Log` as al join `tabAudit Log Items` as ali on ali.parent=al.name join `tabItem` as i on i.item_code=ali.item_code 
+				where i.brand=%(brand)s order by al.posting_date;""",values=values,as_dict=True)
+	sales_data=frappe.db.sql("""select (DATE_FORMAT(si.posting_date,'%%d-%%m-%%y')) as date,si.customer,sii.qty,sii.item_name,i.mrp from `tabSales Invoice` as si join `tabSales Invoice Item` as sii on sii.parent=si.name join `tabItem` as i on i.item_code=sii.item_code 
+				where i.brand=%(brand)s and si.`status` != 'Cancelled' and si.`status`!="Draft" order by si.posting_date;""",values=values,as_dict=True)
+
+	#This function gives list of unique date
+	def get_date(data):
+		list_of_date=[]
+		i=0
+		while i<len(data):
+			al_date=data[i]['date']
+			if al_date in list_of_date:
+				i+=1
+				continue
+			else:
+				list_of_date.append(al_date)
+				i+=1
+		return list_of_date
+
+
+
+	def crate_dict_for_give_date(date,x):
+		customer_dict={}
+		date_date=time.strptime(date,"%d-%m-%y")
+		for i in range(len(x)):
+			doctype_date=time.strptime(x[i]['date'],"%d-%m-%y")
+			if date_date==doctype_date:
+				customer=x[i]['customer']
+				item_name=x[i]['item_name']
+				qty=x[i]['qty']
+				mrp=x[i]['mrp']
+				if customer not in customer_dict:
+					customer_dict[customer]={item_name:[qty,mrp]}
+				else:
+					if item_name not in customer_dict[customer]:
+						customer_dict[customer][item_name]=[qty,mrp]
+		print(customer_dict)
+		return customer_dict
+
+	def get_dictionary_with_date(data):
+		di={}
+		list_of_date=get_date(data)
+		for i,date in enumerate(list_of_date):
+			di[date]=crate_dict_for_give_date(date,data)
+		return di
+	audit_dictionary=get_dictionary_with_date(audit_data)
+	sales_dictionary=get_dictionary_with_date(sales_data)
+
+
+	#merging audit_dictionary and sales_dictionary
+	for i in list(audit_dictionary.keys()):
+		if i not in sales_dictionary:
+			sales_dictionary[i]=audit_dictionary[i]
+		else:
+			customer_list=list(audit_dictionary[i].keys())
+			for j in customer_list:
+				if j not in sales_dictionary[i]:
+					sales_dictionary[i][j]=audit_dictionary[i][j]
+				else:
+					item_list=list(audit_dictionary[i][j].keys())
+					for k in item_list:
+						if k not in sales_dictionary[i][j]:
+							sales_dictionary[i][j][k]=audit_dictionary[i][j][k]
+						else:
+							sales_dictionary[i][j][k][0]=sales_dictionary[i][j][k][0]+audit_dictionary[i][j][k][0]
+	return sales_dictionary
