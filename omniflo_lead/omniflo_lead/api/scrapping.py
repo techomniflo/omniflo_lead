acecb2caeed2276d8b1734126e675f51dc831fa5
gourav-saini-omniflo <gourav.saini@omniflo.in>
2022-07-11 18:49:04 +0530

    refactor

--- omniflo_lead/omniflo_lead/api/scrapping.py
+++ omniflo_lead/omniflo_lead/api/scrapping.py
@@ -4,5 +4,5 @@ import json
 #gives stores data with status and address of it.
 @frappe.whitelist()
 def stores_list_data():
-    stores_list = frappe.db.sql("""select c.customer_name as 'Customer Name', max(vi.creation) as 'Latest Visit log',vi.status, concat(a.address_line1," ",a.address_line2) as address,a.city,a.state,a.country,a.pincode from `tabVisit Log` as vi join `tabCustomer` as c on vi.customer=c.name join `tabAddress` as a on a.name=customer_primary_address where customer is not null and customer!="" group by customer;""",as_list=True)
+    stores_list = frappe.db.sql("""select c.customer_name as 'Customer Name', max(vi.creation) as 'Latest Visit log',vi.status, concat(a.address_line1," ",a.address_line2) as address,a.city,a.state,a.country,a.pincode from `tabVisit Log` as vi join `tabCustomer` as c on vi.customer=c.name join `tabAddress` as a on a.name=customer_primary_address where customer is not null and customer!="" group by customer;""",as_dict=True)
     return stores_list
