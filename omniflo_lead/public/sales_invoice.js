dc532022616f113864e47a5e7e739ecb42f0d224
gourav-saini-omniflo <gourav.saini@omniflo.in>
2022-07-28 17:04:44 +0530

    handling event when customer is null

--- omniflo_lead/public/sales_invoice.js
+++ omniflo_lead/public/sales_invoice.js
@@ -1,13 +1,18 @@
 frappe.ui.form.on('Sales Invoice', {
 
     customer: function(frm){
+        if (frm.doc.customer==""){
+            return 
+        }
         frappe.db.get_doc('Customer',frm.doc.customer).then((values)=>{
             if (values.customer_status!='Live' && values.customer_status!='On-boarded'){
-                frappe.throw("Customer is not Live or not On-boarded its "+values.customer_status+"\n Agent Name"+values.manager_name)
+                frappe.msgprint({message:"Customer is not Live or not On-boarded its "+values.customer_status+" Agent Name  "+values.manager_name,indicator:"red",title:"Error"})
+                frm.set_value("customer", "")
             }
             else{
                 frappe.msgprint("Customer is of Agent Name"+values.manager_name)
             }
+            
         })
     },
     validate: function(frm){
