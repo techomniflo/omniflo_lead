79a13ca39e85bab05d8478a29080c389f57aac12
gourav-saini-omniflo <gourav.saini@omniflo.in>
2022-08-16 18:02:14 +0530

    Delivery Confirmation Receipt  Doctype added

---
+++ omniflo_lead/omniflo_lead/doctype/delivery_confirmation_receipt/delivery_confirmation_receipt.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Omniflo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Delivery Confirmation Receipt', {
+	// refresh: function(frm) {
+
+	// }
+});
