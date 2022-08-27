6c90ecd7cd3942ca972fc624898a5fc71e4429ed
Gourav Saini <gourav.saini@omniflo.in>
2022-08-27 15:49:57 +0530

    doctype reconsilliation changes to reconciliation

--- omniflo_lead/omniflo_lead/doctype/reconciliation/test_reconciliation.py
+++ omniflo_lead/omniflo_lead/doctype/reconciliation/test_reconciliation.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestReconsillation(FrappeTestCase):
+class TestReconciliation(FrappeTestCase):
 	pass
--- omniflo_lead/omniflo_lead/doctype/reconciliation_items/reconciliation_items.py
+++ omniflo_lead/omniflo_lead/doctype/reconciliation_items/reconciliation_items.py
@@ -4,5 +4,5 @@
 # import frappe
 from frappe.model.document import Document
 
-class ReconsillationItems(Document):
+class ReconciliationItems(Document):
 	pass
