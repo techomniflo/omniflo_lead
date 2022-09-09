18dd4fb051956ad941cb70db3e6ef9c35fa06f08
Gourav Saini <gourav.saini@omniflo.in>
2022-09-09 14:23:38 +0530

    syntax error corrected

--- omniflo_lead/merchandise/web_form/promoter_logs/promoter_logs.js
+++ omniflo_lead/merchandise/web_form/promoter_logs/promoter_logs.js
@@ -10,8 +10,8 @@ frappe.ready(function() {
 	function showPosition(position) {
 		console.log(position.coords.longitude)
 		console.log(position.coords.latitude)
-		frappe.web_form.set_value([latitude], [position.coords.latitude]);
-		frappe.web_form.set_value([longitude], [position.coords.longitude]);
+		frappe.web_form.set_value(latitude, position.coords.latitude);
+		frappe.web_form.set_value(longitude, position.coords.longitude);
 	  }
 
 	  frappe.web_form.after_load = () => {
