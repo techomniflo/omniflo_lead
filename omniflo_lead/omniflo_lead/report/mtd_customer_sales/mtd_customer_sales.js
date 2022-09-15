82dc8b265dc8abdefd9e7d4e5fe8827f77c11755
Gourav Saini <gourav.saini@omniflo.in>
2022-09-15 16:23:50 +0530

    Custom script report mtd customer sales added

---
+++ omniflo_lead/omniflo_lead/report/mtd_customer_sales/mtd_customer_sales.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2022, Omniflo and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["MTD Customer Sales"] = {
+	"filters": [
+
+	]
+};
