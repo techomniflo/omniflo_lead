54e4ce4cf91c2309a82baf7de478bb1247f707ff
Gourav Saini <gourav.saini@omniflo.in>
2022-09-21 17:19:47 +0530

    on click add button event added

--- omniflo_lead/merchandise/doctype/discount_sheet_generator/discount_sheet_generator.py
+++ omniflo_lead/merchandise/doctype/discount_sheet_generator/discount_sheet_generator.py
@@ -8,4 +8,44 @@ class DiscountSheetGenerator(Document):
 
 	@frappe.whitelist()
 	def fetch_items(self,brand_offer):
-		frappe.msgprint(brand_offer)
+		values={"customer":self.customer,"brand_offer":brand_offer}
+		fetch_data=frappe.db.sql("""
+		select
+		bo.item_code,bo.item_name,bo.brand,bo.mrp,bo.offer_price,bo.difference,bo.offer_percentage
+		from 
+		`tabBrand Offers Items` as bo 
+		inner join (
+			select 
+			i.item_code, 
+			i.item_name, 
+			i.brand, 
+			i.mrp, 
+			(
+				sum(sii.qty)
+			) as Bill_Qty 
+			from 
+			`tabSales Invoice` as si 
+			join `tabSales Invoice Item` as sii on si.name = sii.parent 
+			join `tabItem` as i on i.item_code = sii.item_code 
+			join `tabCustomer` as c on c.name = si.customer 
+			where 
+			si.company = "Omnipresent Services" 
+			and si.status != 'Cancelled' 
+			and si.status != "Draft" 
+			and si.customer = %(customer)s 
+			group by 
+			i.brand, 
+			si.customer, 
+			i.item_name
+		) as meta on meta.item_code = bo.item_code
+		where bo.parent=%(brand_offer)s """,values=values,as_dict=True)
+		for i in fetch_data:
+			self.append('items',{
+				"item_code":i.item_code,
+				"item_name":i.item_name,
+				"brand":i.brand,
+				"mrp":i.mrp,
+				"offer_price":i.offer_price,
+				"difference":i.difference,
+				"offer_percentage":i.offer_percentage
+			})
