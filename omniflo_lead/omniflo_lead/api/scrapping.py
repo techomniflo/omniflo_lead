3e798e38367feaa7593e2ddd3096f10123b97372
Gourav Saini <gourav.saini@omniflo.in>
2022-09-26 13:03:16 +0530

    added territory field in scrapping in stores data with status.

--- omniflo_lead/omniflo_lead/api/scrapping.py
+++ omniflo_lead/omniflo_lead/api/scrapping.py
@@ -9,7 +9,7 @@ def stores_list_data():
 
 @frappe.whitelist()
 def stores_data_with_status():
-    store_list=frappe.db.sql("""select `tabCustomer`.name as name_id,`tabCustomer`.customer_name,AL.status,(concat (A.address_line1," ",A.address_line2)) as address,A.city,A.state,A.pincode,`tabCustomer`.longitude,`tabCustomer`.latitude from `tabVisit Log` as AL left join `tabCustomer` on `tabCustomer`.name=AL.customer left join `tabDynamic Link` as dl on dl.link_name=`tabCustomer`.name left join `tabAddress` as A on A.name=dl.parent  where AL.modified in (select max(modified) from `tabVisit Log` where AL.customer=customer) and `tabCustomer`.customer_name is not null group by `tabCustomer`.customer_name;""",as_dict=True)
+    store_list=frappe.db.sql("""select `tabCustomer`.name as name_id,`tabCustomer`.customer_name,`tabCustomer`.territory,AL.status,(concat (A.address_line1," ",A.address_line2)) as address,A.city,A.state,A.pincode,`tabCustomer`.longitude,`tabCustomer`.latitude from `tabVisit Log` as AL left join `tabCustomer` on `tabCustomer`.name=AL.customer left join `tabDynamic Link` as dl on dl.link_name=`tabCustomer`.name left join `tabAddress` as A on A.name=dl.parent  where AL.modified in (select max(modified) from `tabVisit Log` where AL.customer=customer) and `tabCustomer`.customer_name is not null group by `tabCustomer`.customer_name;""",as_dict=True)
     return store_list
 
 @frappe.whitelist()
