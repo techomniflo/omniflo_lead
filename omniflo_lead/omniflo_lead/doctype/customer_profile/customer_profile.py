439a9fe54a994bab08ca2d6e8f18150e073bdb67
Gourav Saini <gourav.saini@omniflo.in>
2022-10-10 12:44:09 +0530

    customer profile is updated where it has scrapped data of customer

--- omniflo_lead/omniflo_lead/doctype/customer_profile/customer_profile.py
+++ omniflo_lead/omniflo_lead/doctype/customer_profile/customer_profile.py
@@ -2,21 +2,12 @@
 # For license information, please see license.txt
 
 import frappe
-
+import json
+import copy
 from frappe.model.document import Document
 from frappe.utils.background_jobs import enqueue
 
 from omniflo_lead.omniflo_lead.utilities import qrcode_as_png
 
 class CustomerProfile(Document):
-	def before_save(self):
-		self.create_qr_code()
-	
-	def create_qr_code(self):
-		if self.qr_code:
-			return
-		try:
-			file_url = qrcode_as_png('Customer Profile', self.customer)
-			self.qr_code = file_url
-		except:
-			pass
+	pass
