8df0e1a5f2a55b7e5c758520a220068a503a1db5
Gourav Saini <gourav.saini@omniflo.in>
2022-10-18 18:38:13 +0530

    promoter log on_save  ---> to your response has been submited

--- omniflo_lead/merchandise/web_form/promoter_logs/promoter_logs.py
+++ omniflo_lead/merchandise/web_form/promoter_logs/promoter_logs.py
@@ -3,3 +3,7 @@ import frappe
 def get_context(context):
 	# do your magic here
 	pass
+@frappe.whitelist(allow_guest=True)
+def fetch_item():
+	data=frappe.db.sql("""select c.name from `tabCustomer` as c where c.customer_status='Live' and c.name not in ( select si.customer from `tabSales Invoice` as si where si.company="Omniway Technologies Pvt Ltd" and si.customer=c.name) order by c.name;""",as_list=True)
+	return data
