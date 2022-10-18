1fc31bfe700ce43e71188cfdc9bf058b0a631019
Gourav Saini <gourav.saini@omniflo.in>
2022-10-18 18:48:01 +0530

    on save sucessfull message in page has been showing now

--- omniflo_lead/merchandise/web_form/promoter_sales_capture/promoter_sales_capture.js
+++ omniflo_lead/merchandise/web_form/promoter_sales_capture/promoter_sales_capture.js
@@ -1,6 +1,32 @@
 frappe.ready(function() {
-	
+	function get_live_customer(){
+		frappe.call({
+			method:"omniflo_lead.merchandise.web_form.promoter_logs.promoter_logs.fetch_item",
+			freeze:true,
+			callback: function(r){
+				var res=r.message
+				console.log(res)
+				frappe.web_form.set_df_property('customer', 'options', res)
+			}
+		});
+	}
+	function on_save_event(){
+		var validate=frappe.web_form.validate_section()
+			
+			if (validate==true ) {
+				frappe.web_form.save()
+			frappe.web_form.empty()
+			$("<h1>Your Response has been Submitted</h1>").appendTo(frappe.web_form.page)
+			}
+	}
 	frappe.web_form.after_load = () => {
+		$(".btn-primary").remove()
+		frappe.web_form.add_button("Save","button",on_save_event)
+		save_button=$('button:contains("Save")')
+		save_button.css({"background-color":"Blue","color":"white"})
+
+		get_live_customer()
+
 		frappe.web_form.on('brand', setting_value_item_name )
 		function setting_value_item_name(){
 			frappe.call({
