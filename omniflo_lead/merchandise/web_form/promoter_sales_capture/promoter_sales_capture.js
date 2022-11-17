c98ae0867123e46369804d18af395909a75ac458
Gourav Saini <gourav.saini@omniflo.in>
2022-11-17 14:14:44 +0530

    qty to read only -> write , and item code new field is added.

--- omniflo_lead/merchandise/web_form/promoter_sales_capture/promoter_sales_capture.js
+++ omniflo_lead/merchandise/web_form/promoter_sales_capture/promoter_sales_capture.js
@@ -1,4 +1,38 @@
 frappe.ready(function() {
+
+	function setting_value_item_code(){
+		frappe.call({
+			method:"omniflo_lead.merchandise.web_form.promoter_sales_capture.promoter_sales_capture.fetch_item_code",
+			freeze:true,
+			args:{
+				item_name:frappe.web_form.get_value("item_name"),
+				brand:frappe.web_form.get_value("brand")
+			},
+			callback:function(r){
+				frappe.web_form.set_value('item_code',r.message)
+			}
+		})
+	}
+
+	function setting_value_item_name(){
+		frappe.call({
+			method:"omniflo_lead.merchandise.web_form.promoter_sales_capture.promoter_sales_capture.fetch_item_name",
+			freeze:true,
+
+			args:{
+				brand:frappe.web_form.get_value("brand")
+			},
+			callback: function(r){
+				var res=r.message
+				var arr=[]
+			res.forEach(function (item, index) {
+				arr.push([item["item_name"]])
+				});
+				frappe.web_form.set_df_property('item_name', 'options', arr)
+			}
+		});
+	}
+
 	function get_live_customer(){
 		frappe.call({
 			method:"omniflo_lead.merchandise.web_form.promoter_logs.promoter_logs.fetch_item",
@@ -28,23 +62,7 @@ frappe.ready(function() {
 		get_live_customer()
 
 		frappe.web_form.on('brand', setting_value_item_name )
-		function setting_value_item_name(){
-			frappe.call({
-				method:"omniflo_lead.merchandise.web_form.promoter_sales_capture.promoter_sales_capture.fetch_item",
-				freeze:true,
-
-				args:{
-					brand:frappe.web_form.get_value("brand")
-				},
-				callback: function(r){
-					var res=r.message
-					var arr=[]
-				res.forEach(function (item, index) {
-					arr.push([item["item_name"]])
-					});
-					frappe.web_form.set_df_property('item_name', 'options', arr)
-				}
-			});
-		}
+		frappe.web_form.on('item_name',setting_value_item_code)
+		
 	}
 })
