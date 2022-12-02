fb82facbf4cf37787583c8ddad9dd23d4ced89e2
Gourav Saini <gourav.saini@omniflo.in>
2022-12-02 12:01:17 +0530

    sort by brand

--- omniflo_lead/omniflo_lead/doctype/reconciliation/reconciliation.py
+++ omniflo_lead/omniflo_lead/doctype/reconciliation/reconciliation.py
@@ -13,7 +13,8 @@ class Reconciliation(Document):
 	
 	@frappe.whitelist()
 	def fetch_items(self):
-		bin_items = frappe.get_all('Bin', filters = {'warehouse' : self.default_warehouse}, fields =['name'])
+		# bin_items = frappe.get_all('Bin', filters = {'warehouse' : self.default_warehouse}, fields =['name'])
+		bin_items=frappe.db.sql("""select b.name from `tabBin` as b join `tabItem` as i on b.item_code=i.item_code where b.warehouse= order by i.brand""",values=self.default_warehouse,as_dict=True)
 		current_items = []
 		if self.get('items'):
 			current_items = [item.item_code for item in self.get('items')]
