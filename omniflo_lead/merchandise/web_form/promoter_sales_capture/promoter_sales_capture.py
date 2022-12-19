395cb112bf8d660ea32e57b5180281d3c90c9610
Gourav Saini <gourav.saini@omniflo.in>
2022-12-19 16:31:00 +0530

    show item,brand which is billed to customer

--- omniflo_lead/merchandise/web_form/promoter_sales_capture/promoter_sales_capture.py
+++ omniflo_lead/merchandise/web_form/promoter_sales_capture/promoter_sales_capture.py
@@ -6,13 +6,13 @@ def get_context(context):
 	pass
 
 @frappe.whitelist(allow_guest=True)
-def fetch_item_name(brand):
-    values={"brand":brand}
-    return frappe.db.sql("""select item_name from `tabItem` where brand=%(brand)s""",values=values,as_dict=True)
-
-@frappe.whitelist(allow_guest=True)
-def fetch_item_code(item_name,brand):
-    values={"brand":brand,"item_name":item_name}
-    x=frappe.db.sql("""select name from `tabItem` where brand=%(brand)s  and item_name=%(item_name)s """,values=values,as_dict=True)
-    return x[0]["name"]
-    
+def fetch_billed_details(customer):
+    values={"customer":customer}
+    data=frappe.db.sql("""select i.brand,i.item_name,i.item_code,i.mrp from `tabSales Invoice` as si join `tabSales Invoice Item` as sii on si.name=sii.parent join `tabItem` as i on i.item_code=sii.item_code where si.docstatus=1 and si.customer=%(customer)s and 0<(select sum(SII.qty) from `tabSales Invoice` as SI join `tabSales Invoice Item` as SII on SI.name=SII.parent where SI.docstatus=1 and SI.company=si.company and SI.customer=si.customer and SII.item_code=sii.item_code) group by i.brand,i.item_name,i.item_code,i.mrp""",values=values,as_dict=True)
+    brand_details={}
+    for i in data:
+        if i['brand'] not in brand_details:
+            brand_details[i['brand']]=[(i['item_name'],"mrp:- "+str(i['mrp']),i['item_code'])]
+        else:
+            brand_details[i['brand']].append((i['item_name'],"mrp:- "+str(i['mrp']),i['item_code']))
+    return brand_details
