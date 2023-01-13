5b599569257586253c8fa58b4c7d24a53e75e0c3
Gourav Saini <gourav.saini@omniflo.in>
2023-01-13 16:05:02 +0530

    store order doctype has been made

---
+++ omniflo_lead/merchandise/doctype/store_order/store_order.js
@@ -0,0 +1,20 @@
+// Copyright (c) 2023, Omniflo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Store Order', {
+	// refresh: function(frm) {
+
+	// }
+	get_items(frm){
+		cur_frm.clear_table("items");
+		cur_frm.refresh_field('items');
+		frappe.call({
+			doc : frm.doc,
+			method : 'fetch_items',
+			freeze : true,
+			freeze_message : 'Getting All Items'
+		}).then((res) => {
+				refresh_field('items');
+		})
+	}
+});
