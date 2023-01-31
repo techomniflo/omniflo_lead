f9ad2aaacea56f9db59ea042cacf57eaa97982e4
gouravengineer <gouravenginner@gmail.com>
2023-01-31 13:16:27 +0530

    item group is mapped with promoter

--- omniflo_lead/merchandise/doctype/promoter/promoter.js
+++ omniflo_lead/merchandise/doctype/promoter/promoter.js
@@ -2,7 +2,14 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Promoter', {
-	// refresh: function(frm) {
+	refresh: function(frm) {
+		cur_frm.fields_dict['item_group'].get_query = function(doc) {
+			return {
+				filters: {
+					"name": ['in',['Food & Grocery','Purplle','Personal Care']]
+				}
+			}
+		}
 
-	// }
+	}
 });
