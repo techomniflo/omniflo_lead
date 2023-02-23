5429f6cb2284f5148013ba259ffa0069edd4195d
gouravengineer <gouravenginner@gmail.com>
2023-02-23 18:53:03 +0530

    ignore if brand is sample or tester

--- omniflo_lead/merchandise/web_form/promoter_sales_capture/promoter_sales_capture.py
+++ omniflo_lead/merchandise/web_form/promoter_sales_capture/promoter_sales_capture.py
@@ -8,7 +8,7 @@ def get_context(context):
 @frappe.whitelist(allow_guest=True)
 def fetch_billed_details(customer):
     values={"customer":customer}
-    data=frappe.db.sql("""select i.brand,i.item_name,i.item_code,i.mrp from `tabSales Invoice` as si join `tabSales Invoice Item` as sii on si.name=sii.parent join `tabItem` as i on i.item_code=sii.item_code where si.docstatus=1 and si.customer=%(customer)s and 0<(select sum(SII.qty) from `tabSales Invoice` as SI join `tabSales Invoice Item` as SII on SI.name=SII.parent where SI.docstatus=1 and SI.company=si.company and SI.customer=si.customer and SII.item_code=sii.item_code) group by i.brand,i.item_name,i.item_code,i.mrp""",values=values,as_dict=True)
+    data=frappe.db.sql("""select i.brand,i.item_name,i.item_code,i.mrp from `tabSales Invoice` as si join `tabSales Invoice Item` as sii on si.name=sii.parent join `tabItem` as i on i.item_code=sii.item_code where i.brand not in ('Sample','Tester') and si.docstatus=1 and si.customer=%(customer)s and 0<(select sum(SII.qty) from `tabSales Invoice` as SI join `tabSales Invoice Item` as SII on SI.name=SII.parent where SI.docstatus=1 and SI.company=si.company and SI.customer=si.customer and SII.item_code=sii.item_code) group by i.brand,i.item_name,i.item_code,i.mrp""",values=values,as_dict=True)
     brand_details={}
     for i in data:
         if i['brand'] not in brand_details:
