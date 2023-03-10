6201b81dfa03589b617bdb092139b1a9dc883d3f
gouravengineer <gouravenginner@gmail.com>
2023-03-10 16:39:45 +0530

    added a button in sales invoice which give suggested refil qty

--- omniflo_lead/omniflo_lead/api/planogram_api.py
+++ omniflo_lead/omniflo_lead/api/planogram_api.py
@@ -14,3 +14,29 @@ def return_planogram():
 def find_asset():
     values={"customer":frappe.request.args["customer"]}
     return frappe.db.sql("""select p.name as planogram_id,p.shelf_code as asset_type from `tabPlanogram` as p where p.disabled=0 and p.customer=%(customer)s """,values=values,as_dict=True)
+
+# this function is to find suggested refil qty based on planogram.
+@frappe.whitelist()
+def get_suggested_items(item_group,warehouse,customer):
+	
+	suggested_items=[]
+	if item_group=='All':
+		item_group=""
+	else :
+		item_group=f"""and item_group='{item_group}'"""
+	frappe.msgprint(item_group)
+	db_query=f""" select i.item_code,i.item_name,i.brand,pi.qty as planned_qty,b.actual_qty,(select if (sum(dws.qty)>0,sum(dws.qty),0) from `tabDay Wise Sales` as dws where dws.customer=p.customer and dws.item_code=pi.item_code) as sell,(select if (sum(sii.qty*sii.conversion_factor),sum(sii.qty*sii.conversion_factor),0) from `tabSales Invoice` as si join `tabSales Invoice Item` as sii on si.name=sii.parent where si.customer=p.customer and si.docstatus=1 and sii.item_code=pi.item_code) as billed from `tabPlanogram` as p join `tabPlanogram Items` as pi on p.name=pi.parent join `tabItem` as i on i.item_code=pi.item_code join `tabBin` as b on b.item_code=i.item_code where p.disabled=0 and p.customer='{customer}' {item_group} and b.warehouse='{warehouse}' order by i.item_group,i.brand,i.item_name """
+	items_dict=frappe.db.sql(db_query,as_dict=True)
+	for item in items_dict:
+		item_to_deploy=item['planned_qty']-(item['billed']-item['sell'])
+		if item_to_deploy<1 or item['actual_qty']<1:
+			continue
+		if item['actual_qty']-item_to_deploy<0 and item['actual_qty']>0:
+			item_to_deploy=item['actual_qty']
+		suggested_items.append(
+					{'item_code' : item['item_code'], 
+					'item_name' : item['item_name'],
+					'brand': item['brand'],
+					'qty':item_to_deploy
+					})
+	return suggested_items
---
+++ omniflo_lead/public/sales_order.js
@@ -0,0 +1,68 @@
+frappe.ui.form.on('Sales Order', {
+    setup:function(frm){
+        frm.set_df_property('set_warehouse','read_only',1)
+        frm.doc.set_warehouse="Kormangala WareHouse - OS"
+        frm.doc.company="Omnipresent Services"
+    },
+    refresh: function(frm) {
+        
+        if (frm.doc.docstatus==0){
+            // adding custom button Get Suggested Planogram Qty
+          frm.add_custom_button(__('Get Suggested Planogram Qty'), function(){
+                // defining a Dialog which is popup to take input for suggested qty
+            let d = new frappe.ui.Dialog({
+                title: 'Enter details',
+                fields: [
+                    {
+                        label: 'Item Group',
+                        fieldname: 'item_group',
+                        fieldtype: 'Link',
+                        options: "Item Group",
+                        filters:{'name':["in",['Food & Grocery',"Purplle","Personal Care"]]}
+                    }],
+                    primary_action_label: 'Submit',
+                    primary_action(values) {
+                        // get qty from call
+                        frappe.call({
+                            method: 'omniflo_lead.omniflo_lead.api.planogram_api.get_suggested_items',
+                            freeze:true,
+                            async: false,
+                            args: {
+                                item_group:values.item_group || 'All',
+                                customer:frm.doc.customer,
+                                warehouse:frm.doc.set_warehouse
+                            },
+                            callback: function(r) {
+                                if (!r.exc) {
+                                    
+                                    const array=r.message
+                                    array.forEach(function (suggested_item, index) {
+                                        
+                                        item=frm.add_child('items',{'item_code':suggested_item['item_code'],'qty':suggested_item['qty']})
+                                        if (frm.doc.delivery_date) {
+                                            item.delivery_date = frm.doc.delivery_date;
+                                            
+                                        } else {
+                                            frm.script_manager.copy_from_first_row("items", row, ["delivery_date"]);
+                                        }
+                                        frm.cscript.item_code(frm,'Sales Order Item',item.name)
+                                      });
+                                    
+                                }
+                                
+                                d.hide(); //hide dialog when all appeded all items
+                            }
+                        });
+  
+                        
+                    }
+                
+                })
+            d.show()  //show dialog 
+        });
+
+    }
+
+  }
+
+});
