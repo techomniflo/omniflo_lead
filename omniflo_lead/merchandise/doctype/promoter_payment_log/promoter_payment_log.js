82f9fb0b638a532e7a16ef047c74df5cec8e6ac7
gouravengineer <gouravenginner@gmail.com>
2023-04-14 16:00:49 +0530

    promoter payment log doctype is created

---
+++ omniflo_lead/merchandise/doctype/promoter_payment_log/promoter_payment_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Omniflo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Promoter Payment Log', {
+	// refresh: function(frm) {
+
+	// }
+});
