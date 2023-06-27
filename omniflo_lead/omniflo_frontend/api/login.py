4c397b59266f942a3b594bd4448b10606ae83c61
gouravengineer <gouravenginner@gmail.com>
2023-06-27 17:32:52 +0530

    feat:login api created which return auth key and secret

---
+++ omniflo_lead/omniflo_frontend/api/login.py
@@ -0,0 +1,47 @@
+import frappe
+from frappe import auth
+
+@frappe.whitelist( allow_guest=True )
+def login(usr, pwd):
+    """The function is designed to retrieve the user's API key and API secret, which are required for generating headers to be used in making requests to other APIs. These credentials serve as authentication parameters, ensuring that only authorized users can access the requested APIs. """
+    try:
+        login_manager = frappe.auth.LoginManager()
+        login_manager.authenticate(user=usr, pwd=pwd)
+        login_manager.post_login()
+    except frappe.exceptions.AuthenticationError:
+        frappe.clear_messages()
+        frappe.local.response['http_status_code'] = 401
+        frappe.local.response["message"] = {
+            "success_key":0,
+            "message":"Authentication Error!"
+        }
+
+        return
+
+    api_generate = generate_keys(frappe.session.user)
+    user = frappe.get_doc('User', frappe.session.user)
+
+    frappe.response["message"] = {
+        "success_key":1,
+        "message":"Authentication success",
+        "sid":frappe.session.sid,
+        "api_key":user.api_key,
+        "api_secret":api_generate,
+        "username":user.username,
+        "email":user.email
+    }
+
+
+
+def generate_keys(user):
+    user_details = frappe.get_doc('User', user)
+    api_secret = frappe.generate_hash(length=15)
+
+    if not user_details.api_key:
+        api_key = frappe.generate_hash(length=15)
+        user_details.api_key = api_key
+
+    user_details.api_secret = api_secret
+    user_details.save()
+
+    return api_secret
