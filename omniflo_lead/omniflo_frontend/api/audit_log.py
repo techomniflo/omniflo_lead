bf328275ef0aed29156042f6819d099167090e83
gouravengineer <gouravenginner@gmail.com>
2023-07-05 18:25:39 +0530

    feat:api created for audit app

--- omniflo_lead/omniflo_frontend/api/audit_log.py
+++ omniflo_lead/omniflo_frontend/api/audit_log.py
@@ -101,3 +101,15 @@ def post_audit_log():
         frappe.enqueue(update_audit_images,doc=doc,images=kwargs["images"])
     frappe.local.response['http_status_code'] = 201
     return doc
+
+@frappe.whitelist()
+def pending_audits_list():
+    """ This API provides a list of stores and the number of days since the last audit. """
+    user=frappe.session.user
+    return frappe.db.sql(""" select al.customer,DATEDIFF(curdate(),date(al.posting_date)) days from `tabAudit Log`  as al join (select AL.customer,max(posting_date) as posting_date from `tabAudit Log` as AL where AL.docstatus=1 group by AL.customer) as meta on meta.customer=al.customer and meta.posting_date=al.posting_date join `tabCustomer` as c on c.name=al.customer where c.customer_status='Live' and al.docstatus=1 and al.owner=%(owner)s order by days desc """,values={"owner":user},as_dict=True)
+
+@frappe.whitelist()
+def stores_audited_today():
+    """ This API provides a list of stores that were audited today. """
+    user=frappe.session.user
+    return frappe.db.sql(""" select count(*) as count,al.customer from  `tabAudit Log` as al where al.docstatus=1 and date(al.posting_date)=curdate() and al.owner=%()s group by al.customer """,values={'owner':user},as_dict=True)
