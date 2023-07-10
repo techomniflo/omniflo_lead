ec26424715024c6729ede2fb48a49dba6c281695
gouravengineer <gouravenginner@gmail.com>
2023-07-10 14:48:47 +0530

    feat : Third Party Invoicing doctype is created

---
+++ omniflo_lead/omniflo_lead/doctype/third_party_invoicing/third_party_invoicing.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Omniflo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Third Party Invoicing', {
+	// refresh: function(frm) {
+
+	// }
+});
