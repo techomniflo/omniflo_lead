b47ae689bbd4fb6403e7570aca294267bae4b039
gouravengineer <gouravenginner@gmail.com>
2023-07-14 19:01:47 +0530

    get_item api updated according to sub_brand

--- omniflo_lead/omniflo_frontend/api/promoter_app.py
+++ omniflo_lead/omniflo_frontend/api/promoter_app.py
@@ -146,16 +146,15 @@ def get_customer_location():
 	return frappe.db.sql("select cp.latitude,cp.longitude from `tabCustomer Profile` as cp where cp.customer=%(customer)s",values=values,as_dict=True)
 
 @frappe.whitelist(allow_guest=True)
-def get_items():
-	promoter=frappe.get_doc('Promoter',frappe.request.args["promoter"])
-	values={"customer":frappe.request.args["customer"],"item_group":promoter.item_group}
-	data=frappe.db.sql("""select i.brand,i.item_name,i.item_code,i.mrp from `tabSales Invoice` as si join `tabSales Invoice Item` as sii on si.name=sii.parent join `tabItem` as i on i.item_code=sii.item_code where i.brand not in ('Sample','Tester') and i.item_group=%(item_group)s and si.docstatus=1 and si.customer=%(customer)s and 0<(select sum(SII.qty) from `tabSales Invoice` as SI join `tabSales Invoice Item` as SII on SI.name=SII.parent where SI.docstatus=1 and SI.company=si.company and SI.customer=si.customer and SII.item_code=sii.item_code) group by i.brand,i.item_name,i.item_code,i.mrp""",values=values,as_dict=True)
+def get_items(customer):
+	values={"customer":customer}
+	data=frappe.db.sql("""select i.sub_brand,i.item_name,i.item_code,i.mrp from `tabSales Invoice` as si join `tabSales Invoice Item` as sii on si.name=sii.parent join `tabItem` as i on i.item_code=sii.item_code where i.brand not in ('Sample','Tester') and si.docstatus=1 and si.customer=%(customer)s and 0<(select sum(SII.qty) from `tabSales Invoice` as SI join `tabSales Invoice Item` as SII on SI.name=SII.parent where SI.docstatus=1 and SI.company=si.company and SI.customer=si.customer and SII.item_code=sii.item_code) group by i.sub_brand,i.item_name,i.item_code,i.mrp""",values=values,as_dict=True)
 	brand_details={}
 	for i in data:
-		if i['brand'] not in brand_details:
-			brand_details[i['brand']]=[{"item_name":i['item_name'],"mrp":i['mrp'],"item_code":i['item_code']}]
+		if i['sub_brand'] not in brand_details:
+			brand_details[i['sub_brand']]=[(i['item_name'],"mrp:- "+str(i['mrp']),i['item_code'])]
 		else:
-			brand_details[i['brand']].append({"item_name":i['item_name'],"mrp":i['mrp'],"item_code":i['item_code']})
+			brand_details[i['sub_brand']].append((i['item_name'],"mrp:- "+str(i['mrp']),i['item_code']))
 	return brand_details
 
 @frappe.whitelist(allow_guest=True)
