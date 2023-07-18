5577b0a75e34c3731a5f8a06c53c0879053da08f
gouravengineer <gouravenginner@gmail.com>
2023-07-18 18:08:41 +0530

    only show one image for one day for each image type

--- omniflo_lead/merchandise/page/hygiene_approver/hygiene_approver.py
+++ omniflo_lead/merchandise/page/hygiene_approver/hygiene_approver.py
@@ -5,7 +5,7 @@ import json
 def get_details(type=""):
     if type!="":
         type=f"""and php.type='{type}'"""
-    image_details = frappe.db.sql(""" select php.name as photos_doc_name,php.creation,php.image,php.type,ph.*,pl.customer,pl.promoter,pl.item_group from `tabPromoter Hygiene` as ph join `tabPromoter Hygiene Photos` as php on ph.name=php.parent join `tabPromoter Log` as pl on pl.name=ph.promoter_log where php.type is not null and (php.status is null or php.status='') """+type,as_dict=True)
+    image_details = frappe.db.sql(""" select php.name as photos_doc_name,php.creation,php.image,php.type,ph.*,pl.customer,pl.promoter,pl.item_group from `tabPromoter Hygiene` as ph join `tabPromoter Hygiene Photos` as php on ph.name=php.parent join `tabPromoter Log` as pl on pl.name=ph.promoter_log where php.type is not null and (php.status is null or php.status='') and ph.creation in (select min(PH.creation) from `tabPromoter Hygiene` as PH join `tabPromoter Log` as PL on PL.name=PH.promoter_log where PL.promoter=pl.promoter and date(PH.creation)=date(ph.creation) ) """+type,as_dict=True)
     return image_details
 
 @frappe.whitelist()
