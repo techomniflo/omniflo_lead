8a586173cdb2b7b29aa20a6a60fd9767acb9f0fe
gouravsaini021 <gouravenginner@gmail.com>
2023-08-03 17:46:52 +0530

    sales order efficiency doctype is created

---
+++ omniflo_lead/omniflo_lead/doctype/sales_order_efficiency/sales_order_efficiency.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Omniflo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sales Order Efficiency', {
+	// refresh: function(frm) {
+
+	// }
+});
