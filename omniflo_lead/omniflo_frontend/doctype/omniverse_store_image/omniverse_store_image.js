33600fcbe0884ad2970c8280aafda281868cb9b6
gouravsaini021 <gouravenginner@gmail.com>
2023-08-21 18:31:52 +0530

    chore:omniverse store image doctype created

---
+++ omniflo_lead/omniflo_frontend/doctype/omniverse_store_image/omniverse_store_image.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Omniflo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Omniverse Store Image', {
+	// refresh: function(frm) {
+
+	// }
+});
