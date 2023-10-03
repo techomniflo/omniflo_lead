f1894febb5c0f8f4b1bf37e8bea19c6577525556
gouravsaini021 <gouravenginner@gmail.com>
2023-10-03 19:05:05 +0530

    feat:backup for day wise sales

---
+++ omniflo_lead/omniflo_lead/doctype/backup_day_wise_sales/backup_day_wise_sales.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Omniflo and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Backup Day Wise Sales', {
+	// refresh: function(frm) {
+
+	// }
+});
