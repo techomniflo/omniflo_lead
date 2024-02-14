cb4df19408b52b752392df3c41bedd02a7e5b8e3
gouravsaini021 <gouravenginner@gmail.com>
2024-02-14 14:11:13 +0530

    api update show result as dict instead of list

--- omniflo_lead/omniflo_frontend/api/utils.py
+++ omniflo_lead/omniflo_frontend/api/utils.py
@@ -2,5 +2,46 @@ import frappe
 
 @frappe.whitelist(allow_guest=True)
 def get_customer():
-	data=frappe.db.sql("""select c.name from `tabCustomer` as c where c.customer_status in ('Live','Pseudo Live') and c.name not in ( select si.customer from `tabSales Invoice` as si where si.company="Omniway Technologies Pvt Ltd" and si.customer=c.name) order by c.name;""",as_list=True)
+	data=frappe.db.sql("""select 
+							* 
+							from 
+							(
+								select 
+								c.name as customer_id, 
+								c.customer_name,
+								c.customer_status
+								from 
+								`tabCustomer` as c 
+								where 
+								c.customer_status in ('Live', 'Pseudo Live') 
+								and c.name not in (
+									select 
+									si.customer 
+									from 
+									`tabSales Invoice` as si 
+									where 
+									si.company = "Omniway Technologies Pvt Ltd" 
+									and si.customer = c.name
+								)
+							) as customer 
+							left join (
+								select 
+								dl.link_name, 
+								a.address_line1, 
+								a.address_line2, 
+								a.state, 
+								a.pincode 
+								from 
+								`tabDynamic Link` as dl 
+								join `tabAddress` as a on a.name = dl.parent 
+								where 
+								dl.link_doctype = 'Customer' 
+								and a.disabled = 0 
+								and dl.parenttype = 'Address' 
+								group by 
+								dl.link_name
+							) as address on customer.customer_id = address.link_name 
+							order by 
+							customer.customer_id
+							""",as_dict=True)
 	return data
